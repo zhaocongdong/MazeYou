7c86f000a38f59e44ecc1d01667919dc284d95f3
zhaocongdong <zhaocongdong@hotmail.com>
2017-07-19 10:42:53 +0800

    Add file

---
+++ maze.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+
+class Maze():
+    ''' Maze '''
+
+    def __init__(self, maze_list):
+        self.maze_list = maze_list
+        halfl = len(self.maze_list) / 2
+        self.__col, self.__row, self.__maze_path = halfl, halfl, [(halfl, halfl)] 
+        # start point is 1
+        self.maze_list[self.__row][self.__col] = 1
+
+    def across_maze(self):
+        ''' across_maze -> list '''
+        if self.maze_list is None:
+            return []
+        else:
+            while (0 < self.__col < len(self.maze_list)) and (0 < self.__row < len(self.maze_list)):
+                
+                is_across = False
+                if   self.maze_list[self.__row][self.__col + 1] == 0:
+                    self.__col += 1
+                    is_across = True
+                elif self.maze_list[self.__row + 1][self.__col] == 0:
+                    self.__row += 1
+                    is_across = True
+                elif self.maze_list[self.__row][self.__col - 1] == 0:
+                    self.__col -= 1
+                    is_across = True
+                elif self.maze_list[self.__row - 1][self.__col] == 0:
+                    self.__row -= 1
+                    is_across = True
+                else:
+                    pass
+
+                if is_across:
+                    self.__maze_path.append((self.__row, self.__col))
+                    
+                else:
+                    self.maze_list[self.__row][self.__col] = 1
+            return self.__maze_path
+
+'''
+0 1 1 1 1 1 1 1 1 1 1
+0 1 0 1 0 0 0 0 0 0 0
+0 1 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 0 0 1 0 0 0
+0 1 0 1 1 0 0 1 1 0 0
+0 1 0 1 0 0 0 0 0 0 0
+0 1 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 0 0 1 0 0 0
+0 1 0 1 1 1 0 1 1 0 0
+0 1 0 1 0 0 0 0 0 0 0
+'''
