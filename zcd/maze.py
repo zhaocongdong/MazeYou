9479eed770934e8f93390d00f4affc58bd76ed32
zcd40267 <zcd40267@ly.com>
2017-07-19 15:49:01 +0800

    Louis done!

--- zcd/maze.py
+++ zcd/maze.py
@@ -8,14 +8,13 @@ class Maze():
         self.__col, self.__row, self.__maze_path = halfl, halfl, [(halfl, halfl)] 
         # start point is 1
         self.maze_list[self.__row][self.__col] = 1
-
+    
     def across_maze(self):
         ''' across_maze -> list '''
         if self.maze_list is None:
             return []
         else:
-            while (0 < self.__col < len(self.maze_list)) and (0 < self.__row < len(self.maze_list)):
-                
+            while (0 < self.__col < len(self.maze_list[0]) - 1) and (0 < self.__row < len(self.maze_list) - 1):
                 is_across = False
                 if   self.maze_list[self.__row][self.__col + 1] == 0:
                     self.__col += 1
@@ -34,14 +33,36 @@ class Maze():
 
                 if is_across:
                     self.__maze_path.append((self.__row, self.__col))
-                    
+                    self.maze_list[self.__row][self.__col] = 1
                 else:
                     self.maze_list[self.__row][self.__col] = 1
+                    # back
+                    self.__maze_path.pop()
+                    # update postion
+                    if len(self.__maze_path) > 0:
+                        self.__row, self.__col = self.__maze_path[-1]
+                    else:
+                        return self.__maze_path
             return self.__maze_path
 
+def main():
+    ''' main '''
+    arraydd = [[0, 1, 1, 1, 1],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 1, 1, 1, 0]]
+    for row in arraydd:
+        for col in row:
+            print col,
+        print
+    maze = Maze(arraydd)
+    print maze.across_maze()
+
+main()
 '''
 0 1 1 1 1 1 1 1 1 1 1
-0 1 0 1 0 0 0 0 0 0 0
+0 1 0 1 2 2 1 0 0 0 0
 0 1 1 1 1 1 1 1 1 1 1
 0 1 1 1 1 1 1 1 1 1 1
 0 1 1 1 1 0 0 1 0 0 0
