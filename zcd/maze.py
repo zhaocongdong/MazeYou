f657f2dfb01f61fe26ad4ec8afb618191b7afeb5
zcd40267 <zcd40267@ly.com>
2017-07-20 10:10:30 +0800

    review

--- zcd/maze.py
+++ zcd/maze.py
@@ -1,47 +1,48 @@
+''' maze'''
 #! /usr/bin/env python
 import random
 
 def main():
     ''' main '''
-    arraydd = random_list(5, 9)
-    print_list(arraydd)
-    maze = Maze(arraydd)
+    maze = Maze(15, 19)
+    maze.print_list()
     print maze.across_maze()
     maze.across_map()
 
-
-def random_list(start=5, end=5):
-    ''' create list '''
-    row = random.randint(start, end)
-    col = random.randint(start, end)
-    # row, col = 8, 8
-    listdd = []
-    for r in range(0, row):
-        col_list = []
-        for c in range(0, col):
-            col_list.append(random.randint(0, 1))
-        listdd.append(col_list)
-    return listdd
-
-def print_list(array):
-    ''' print list @param [] '''
-    print len(array), 'x', len(array[0])
-    for row in array:
-        for col in row:
-            print col,
-        print
-
-class Maze():
+class Maze(object):
     ''' Maze '''
-    def __init__(self, maze_list):
-        self.maze_list = maze_list
+    def __init__(self, start=5, end=9):
+        self.maze_list = self.random_list(start, end)
         half_row = 0 if self.maze_list is None else len(self.maze_list) / 2
         half_col = 0 if self.maze_list is None or self.maze_list[0] is None else len(self.maze_list[0]) / 2
         self.__row, self.__col = half_row, half_col
         self.__maze_path = [(half_row, half_col)]
         # start point is 1
         self.maze_list[self.__row][self.__col] = 1
-    
+
+    def random_list(self, start=5, end=5):
+        ''' create list '''
+        row = random.randint(start, end)
+        col = random.randint(start, end)
+        # row, col = 8, 8
+        listdd = []
+        for r in range(0, row):
+            col_list = []
+            for c in range(0, col):
+                col_list.append(random.randint(0, 1))
+            listdd.append(col_list)
+        return listdd
+
+    def print_list(self, array=None):
+        ''' print list @param [] '''
+        if array is None:
+            array = self.maze_list
+        print len(array), 'x', len(array[0])
+        for row in array:
+            for col in row:
+                print col,
+            print
+
     def across_maze(self):
         ''' across_maze -> list '''
         if self.maze_list is None:
@@ -84,14 +85,25 @@ class Maze():
     def across_map(self):
         ''' print map list '''
         if self._len_maze_path() > 0:
-            map_list = [] * len(self.maze_list)
-            
+            map_list = self.map_path_list()
             for point in self.__maze_path:
                 row, col = point
                 map_list[row][col] = 0
-            print_list(map_list)
+            self.print_list(map_list)
         else:
             print 'block maze'
+
+    def map_path_list(self):
+        ''' create list '''
+        row = len(self.maze_list)
+        col = len(self.maze_list[0])
+        listdd = []
+        for r in range(0, row):
+            col_list = []
+            for c in range(0, col):
+                col_list.append(1)
+            listdd.append(col_list)
+        return listdd
 
 
 main()
